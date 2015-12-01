77a1afd98ddad4bf2f5232b4f54fad2c0a0344ba
Spencer <scarvill91@gmail.com>
2015-12-01 15:17:50 -0600

    Refactor tests

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -1,97 +1,92 @@
 (ns gilded-rose.core-spec
 (:require [speclj.core :refer :all]
-          [gilded-rose.core :refer [update-quality]]))
+          [gilded-rose.core :refer :all]))
 
 (defn update-item [item] (first (update-quality [item])))
 
 (describe "gilded rose"
-  (let [ vest {:name "+5 Dexterity Vest" :sell-in 10 :quality 20}
-         elixir {:name "Elixir of the Mongoose" :sell-in 5 :quality 7}
-         brie {:name "Aged Brie" :sell-in 2 :quality 0}
-         pass {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 15 :quality 20}
-         sulfuras {:name "Sulfuras, Hand of Ragnaros" :sell-in 0 :quality 80} ]
+  (let
+    [
+      vest      (item "+5 Dexterity Vest" 10 20)
+      elixir    (item "Elixir of the Mongoose" 5 7)
+      brie      (item "Aged Brie" 2 0)
+      passes    (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
+      sulfuras  (item "Sulfuras, Hand of Ragnaros" 0 80)
+    ]
 
     (describe "update-quality"
 
-      ; Should pass according to behavior specified in README, but doesn't
-      ; (it "never decreases quality past zero"
-      ;   (let [item {:name "item" :sell-in 10 :quality 0}]
-      ;     (should= 0 (:quality (update-quality item)))))
+      (it "never decreases quality past zero"
+        (pending "Should pass according to behavior specified in README, but doesn't due to bug")
+        (let [vest (assoc vest :quality 0) elixir (assoc elixir :quality 0)]
+          (should= 0 (:quality (update-item vest)))
+          (should= 0 (:quality (update-item elixir)))))
 
       (it "never increases quality above fifty"
-        (let [brie (assoc brie :quality 50)]
-          (should= 50 (:quality (update-item brie)))))
+        (let [brie (assoc brie :quality 50) passes (assoc passes :quality 50)]
+          (should= 50 (:quality (update-item brie)))
+          (should= 50 (:quality (update-item passes)))))
 
       (context "when item is legendary"
+
         (it "leaves quality and sell-in date unchanged"
-          (should (and (= (:quality sulfuras)
-                          (:quality (update-item sulfuras)))
-                       (= (:sell-in sulfuras)
-                          (:sell-in (update-item sulfuras))))))
+          (should= (:quality sulfuras) (:quality (update-item sulfuras)))
+          (should= (:sell-in sulfuras) (:sell-in (update-item sulfuras))))
       )
 
       (context "when item is not legendary"
 
         (it "decrements sell-in date by one"
-          (let [item {:name "item" :sell-in 10 :quality 20}]
-            (should= (- (:sell-in item) 1)
-                     (:sell-in (first (update-quality [item]))))))
+          (should= (- (:sell-in vest) 1) (:sell-in (update-item vest)))
+          (should= (- (:sell-in elixir) 1) (:sell-in (update-item elixir)))
+          (should= (- (:sell-in brie) 1) (:sell-in (update-item brie)))
+          (should= (- (:sell-in passes) 1) (:sell-in (update-item passes))))
 
         (it "increments brie quality by one"
-          (should= (+ (:quality brie) 1)
-                   (:quality (update-item brie))))
+          (should= (+ (:quality brie) 1) (:quality (update-item brie))))
 
         (context "when item sell-in date has not been reached"
 
           (it "decrements vest quality by one"
-            (should= (- (:quality vest) 1)
-                     (:quality (update-item vest))))
+            (let [vest (assoc vest :sell-in 10)]
+              (should= (- (:quality vest) 1) (:quality (update-item vest)))))
 
           (it "decrements elixir quality by one"
-            (should= (- (:quality elixir) 1)
-                     (:quality (update-item elixir))))
+            (let [elixir (assoc elixir :sell-in 10)]
+              (should= (- (:quality elixir) 1) (:quality (update-item elixir)))))
 
           (context "when sell-in date is greater than 10"
 
-            (it "increments backstage pass quality by one"
-              (should= (+ (:quality pass) 1)
-                       (:quality (update-item pass))))
+            (it "increments backstage passes quality by one"
+              (let [passes (assoc passes :sell-in 15)]
+                (should= (+ (:quality passes) 1) (:quality (update-item passes)))))
           )
 
           (context "when sell-in date is greater than 5 and less than or equal to 10"
 
-            (it "increments backstage pass quality by two"
-              (let [pass (assoc pass :sell-in 10)]
-                (should= (+ (:quality pass) 2)
-                         (:quality (update-item pass)))))
+            (it "increments backstage passes quality by two"
+              (let [passes (assoc passes :sell-in 10)]
+                (should= (+ (:quality passes) 2) (:quality (update-item passes)))))
           )
 
           (context "when sell-in date is greater than 0 and less than or equal to 5"
 
-            (it "increments backstage pass quality by three"
-              (let [pass (assoc pass :sell-in 5)]
-                (should= (+ (:quality pass) 3)
-                         (:quality (update-item pass)))))
-          )
-        )
+            (it "increments backstage passes quality by three"
+              (let [passes (assoc passes :sell-in 5)]
+                (should= (+ (:quality passes) 3) (:quality (update-item passes)))))
+          ))
 
-        (context "when sell-in date has passed"
+        (context "when sell-in date has passesed"
 
           (it "decrements vest quality by two"
             (let [vest (assoc vest :sell-in 0)]
-              (should= (- (:quality vest) 2)
-                       (:quality (update-item vest)))))
+              (should= (- (:quality vest) 2) (:quality (update-item vest)))))
 
           (it "decrements elixir quality by two"
             (let [elixir (assoc elixir :sell-in 0)]
-              (should= (- (:quality elixir) 2)
-                       (:quality (update-item elixir)))))
+              (should= (- (:quality elixir) 2) (:quality (update-item elixir)))))
 
-          (it "sets backstage pass quality to zero"
-            (let [pass (assoc pass :sell-in 0)]
-              (should= 0 (:quality (update-item pass)))))
-        )
-      )
-    )
-  )
-)
+          (it "sets backstage passes quality to zero"
+            (let [passes (assoc passes :sell-in 0)]
+              (should= 0 (:quality (update-item passes)))))
+        )))))
